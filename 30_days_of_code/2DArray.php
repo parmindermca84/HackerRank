5fa2668f311048317140962fad037c711751a705
Parminder Singh <parmindermca84@gmail.com>
2016-08-28 17:38:48 +0530

    Day 11: 2D Arrays
    Objective
    Today, we're building on our knowledge of Arrays by adding another dimension. Check out the Tutorial tab for learning materials and an instructional video!
    
    Context
    Given a 2D Array, :
    
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    0 0 0 0 0 0
    
    We define an hourglass in to be a subset of values with indices falling in this pattern in 's graphical representation:
    
    a b c
      d
    e f g
    
    There are hourglasses in , and an hourglass sum is the sum of an hourglass' values.
    
    Task
    Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
    
    Input Format
    
    There are lines of input, where each line contains space-separated integers describing 2D Array ; every value in will be in the inclusive range of to .
    
    Constraints
    
    Output Format
    
    Print the largest (maximum) hourglass sum found in .
    
    Sample Input
    
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 2 4 4 0
    0 0 0 2 0 0
    0 0 1 2 4 0
    
    Sample Output
    
    19
    
    Explanation
    
    contains the following hourglasses:
    
    1 1 1   1 1 0   1 0 0   0 0 0
      1       0       0       0
    1 1 1   1 1 0   1 0 0   0 0 0
    
    0 1 0   1 0 0   0 0 0   0 0 0
      1       1       0       0
    0 0 2   0 2 4   2 4 4   4 4 0
    
    1 1 1   1 1 0   1 0 0   0 0 0
      0       2       4       4
    0 0 0   0 0 2   0 2 0   2 0 0
    
    0 0 2   0 2 4   2 4 4   4 4 0
      0       0       2       0
    0 0 1   0 1 2   1 2 4   2 4 0
    
    The hourglass with the maximum sum () is:
    
    2 4 4
      2
    1 2 4

---
+++ 30_days_of_code/2DArray.php
@@ -0,0 +1,62 @@
+<?php
+
+$handle = fopen ("php://stdin","r");
+$arr = array();
+for($arr_i = 0; $arr_i < 6; $arr_i++) {
+   $arr_temp = fgets($handle);
+   $arr[] = explode(" ",$arr_temp);
+  array_walk($arr[$arr_i],'intval');
+}
+
+
+$result = [];
+$centerIndex = 1;
+for ($arr_i = 0; $arr_i < 16; $arr_i++) {
+    
+    if ($centerIndex > 4) {
+        $centerIndex = 1;
+    }
+    
+    // Left index always start from 0 to complete first row
+    $leftIndex = 0;
+    $rightIndex = $arr_i;
+    
+    if ($arr_i >= 4) {
+        $leftIndex = intval($arr_i / 4);
+        
+        if ($arr_i % 4 == 0) {
+            $rightIndex = 0;
+        } else {
+            $rightIndex = $rightStartIndex;
+        }
+    }
+    
+    $rightStartIndex = $rightIndex;
+    
+    $result[$arr_i] += FillResult($arr, $leftIndex, $rightIndex);
+    
+    $leftIndex++;
+    
+    $result[$arr_i] += $arr[$leftIndex][$centerIndex];
+    
+    $leftIndex++;
+    
+    $result[$arr_i] += FillResult($arr, $leftIndex, $rightIndex);
+        
+    $centerIndex++;
+    $rightStartIndex++;
+}
+
+function FillResult($arr, $leftIndex, $rightIndex) {
+    $result = 0;
+    
+    for ($i = 0; $i < 3; $i++) {
+        $result += $arr[$leftIndex][$rightIndex];
+        $rightIndex++;
+    }
+    
+    return $result;
+}
+
+echo max($result);
+?>
